a5932335d23e55912ffb9a9ebfb4154a4b40d67e
Marcin Licznerski <marcin.licznerski@iterative.pl>
2026-01-03 13:11:53 +0100

    Add NgxMatTimepickerTimeLocalizerPipe to fix missing pipe error

--- projects/ngx-mat-timepicker/src/lib/components/ngx-mat-timepicker-dial-control/ngx-mat-timepicker-dial-control.component.ts
+++ projects/ngx-mat-timepicker/src/lib/components/ngx-mat-timepicker-dial-control/ngx-mat-timepicker-dial-control.component.ts
@@ -18,7 +18,7 @@ function retainSelection(this: HTMLInputElement) {
     selector: "ngx-mat-timepicker-dial-control",
     templateUrl: "ngx-mat-timepicker-dial-control.component.html",
     styleUrls: ["ngx-mat-timepicker-dial-control.component.scss"],
-    providers: [NgxMatTimepickerParserPipe],
+    providers: [NgxMatTimepickerParserPipe, NgxMatTimepickerTimeLocalizerPipe],
     imports: [
         NgIf,
         FormsModule,
